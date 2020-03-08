cf8b76e57651921857d824ee1fddc9c37e21f3ed
Steven Hertz <hertzsteven@icloud.com>
2020-03-07 22:08:54 -0500

    Initial Commit

---
+++ Apps Process/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Apps Process
+//
+//  Created by Steven Hertz on 3/7/20.
+//  Copyright © 2020 DevelopItSolutions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
